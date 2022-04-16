c667c48b1d5809d60cddec053851b26bce2f5c3f
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2022-04-17 00:06:32 +0600

    init-firebase

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCB3Mx91fQqazlL-Hr1CtYkJzqF1lA4njI",
+  authDomain: "assignment10-b1a48.firebaseapp.com",
+  projectId: "assignment10-b1a48",
+  storageBucket: "assignment10-b1a48.appspot.com",
+  messagingSenderId: "845505551972",
+  appId: "1:845505551972:web:4c74d6a40843cdc4380971"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export default app
