44389976ccd62cf7e8638b35cf9573c617ef6594
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2022-04-17 19:12:59 +0600

    about-create

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './About.css'
+import image from '../../images/33522.jpg'
 
 const About = () => {
     return (
         <div>
-            <h1>about</h1>
+            <h1 className='Heading'>About Me</h1>
+            <div className='Details'>
+                 <img className='Image' src={image} alt=""/>
+                 <h1>Sultan Razib Uddin</h1>
+                 <h3>Goal: Full Stack Web Developer</h3>
+            </div>
         </div>
     );
 };
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
+        <div className='footer'>
             <h1>fooooooter</h1>
         </div>
     );
